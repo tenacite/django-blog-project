ed37bba1cc873f27ff2456282dca73bbf826c324
Nii Logoligi <niilogoligi@niilogoligi-Satellite-C655.(none)>
2012-07-05 12:10:38 +0000

    complete

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+	title=models.CharField(max_length=60)
+	body=models.TextField()
+	created=models.DateField()
+	updated=models.DateField()
+	def __unicode__(self):
+		return self.title
+class Comment(models.Model):
+	body=models.TextField()
+	author=models.CharField(max_length=60)
+	created=models.DateField()
+	updated=models.DateField()
+	post=models.ForeignKey(Post,related_name='Posts')
+	
